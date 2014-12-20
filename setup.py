79cb8a5db3bdb6f859dbc346d7da5931d37571b9
Yusuke Tsutsumi <tsutsumi.yusuke@gmail.com>
2014-12-20 11:44:41 -0700

    revving uranium to 0.0.10

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ tests_require = [
 ]
 
 setup(name='uranium',
-      version='0.0.9',
+      version='0.0.10',
       description='a build system for python',
       long_description='a build system for python',
       author='Yusuke Tsutsumi',
