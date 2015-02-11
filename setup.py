29b00215256a5a6ac7facbda922144f40eeb2b06
Yusuke Tsutsumi <tsutsumi.yusuke@gmail.com>
2015-02-10 19:32:34 -0800

    fixing bin to activate virtualenv

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ tests_require = [
 ]
 
 setup(name='uranium',
-      version='0.0.52',
+      version='0.0.55',
       description='a build system for python',
       long_description='a build system for python',
       author='Yusuke Tsutsumi',
--- uranium/bin.py
+++ uranium/bin.py
@@ -1,6 +1,8 @@
 BIN_SCRIPT_TEMPLATE = """
-import os
+#!/usr/bin/env {python_version}
+import os; activate_this=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'activate_this.py'); exec(compile(open(activate_this).read(), activate_this, 'exec'), dict(__file__=activate_this)); del os, activate_this
 
+import os
 bin_directory = os.path.dirname(os.path.realpath(__file__))
 base = os.path.join(bin_directory, os.pardir)
 {extras}
@@ -8,6 +10,8 @@ base = os.path.join(bin_directory, os.pardir)
 """.strip()
 import os
 import stat
+import sys
+
 
 class BinDirectory(object):
     """ this wraps a few convenience methods for the bin directory """
@@ -25,7 +29,8 @@ class BinDirectory(object):
 
         bin_script = BIN_SCRIPT_TEMPLATE.format(
             body=body,
-            extras=extras
+            extras=extras,
+            python_version=self._get_python_version()
         )
 
         target_path = os.path.join(self.root, name)
@@ -34,6 +39,12 @@ class BinDirectory(object):
             fh.write(bin_script)
 
         _make_executable(target_path)
+
+    def _get_python_version(self):
+        return "python{major}.{minor}".format(
+            major=sys.version_info[0],
+            minor=sys.version_info[1]
+        )
 
 
 def _make_executable(path):
