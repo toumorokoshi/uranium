578b27e4c342154c5e8b5bafb4d95f556dbebfbc
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2016-01-02 20:15:39 -0800

    deleting tests.

--- uranium/tests/config/test_load.py
+++
@@ -1,8 +0,0 @@
-from uranium.config import Config
-
-
-class TestConfigLoad(object):
-
-    def test_load_empty_string(self):
-        """ make sure that loading an empty string works """
-        Config.load_from_string("")
