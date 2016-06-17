e7abc13762477300b18562c2a26cc37cfed13fd6
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2016-06-17 09:10:11 -0700

    v0.2.37

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ tests_require = [
 ]
 
 setup(name='uranium',
-      version='0.2.37b',
+      version='0.2.38b',
       description='a build system for python',
       long_description=open('README.rst').read(),
       author='Yusuke Tsutsumi',
