b2efe53de659f8eede6d9bacb2898f5ba44cb83c
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2016-06-27 01:27:07 -0700

    fixing up standlone script, moving setuptools import to a safer place

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ tests_require = [
 ]
 
 setup(name='uranium',
-      version='0.2.43b',
+      version='0.2.44b',
       description='a build system for python',
       long_description=open('README.rst').read(),
       author='Yusuke Tsutsumi',
